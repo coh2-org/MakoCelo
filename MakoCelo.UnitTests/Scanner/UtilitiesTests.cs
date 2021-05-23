b9ad94e780bfd6c943c03675e6dc01e7e3be4362
Karol Kapuściński <kmkapuscinski@gmail.com>
2021-05-23 22:09:50 +0200

    UnitTest Stub

---
+++ MakoCelo.UnitTests/Scanner/UtilitiesTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MakoCelo.UnitTests.Scanner
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [Test]
+        public void FindPlayerNameInLine()
+        {
+            MakoCelo.Scanner.Utilities.FindPlayerNameInLine("a", 0);
+
+
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
