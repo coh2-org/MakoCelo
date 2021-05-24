a4d164c3800e49223cf2ef2fcb65e1ebaf890c8d
Karol Kapuściński <kmkapuscinski@gmail.com>
2021-05-24 21:55:59 +0200

    UtilitiesTests

--- MakoCelo.UnitTests/Scanner/UtilitiesTests.cs
+++ MakoCelo.UnitTests/Scanner/UtilitiesTests.cs
@@ -1,19 +1,51 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace MakoCelo.UnitTests.Scanner
 {
     [TestFixture]
     public class UtilitiesTests
     {
-        [Test]
-        public void FindPlayerNameInLine()
+        [TestCaseSource(nameof(GetFindPlayerNameInLineTestCases))]
+        public void FindPlayerNameInLine_CorrectLinesFromLog_PlayerNameFound(string line, string expectedResult)
         {
-            MakoCelo.Scanner.Utilities.FindPlayerNameInLine("a", 0);
-
-
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            //Arrange
+            //Act
+            var result = MakoCelo.Scanner.Utilities.FindPlayerNameInLine(line, 39);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
         }
+
+        public static IEnumerable<object> GetFindPlayerNameInLineTestCases()
+        {
+            var lines =TestCases.testcases.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var result = line.Split(';');
+                yield return new string[] { result[0], result[1] };
+            }
+        }
+
+        [TestCaseSource(nameof(GetFindPlayerRelicIdInLineTestCases))]
+        public void FindPlayerRelicIdInLine_CorrectLinesFromLog_RelicIdFound(string line, string expectedResult)
+        {
+            //Arrange
+            //Act
+            var result = MakoCelo.Scanner.Utilities.FindPlayerRelicIdInLine(line);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        public static IEnumerable<object> GetFindPlayerRelicIdInLineTestCases()
+        {
+            var lines = TestCases.testcases.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var result = line.Split(';');
+                yield return new string[] { result[0], result[2] };
+            }
+        }
+
     }
 }
